397714fb29d8faa742e69134867c4689c99d4261
zzo2741 <dlwnstmd@gmail.com>
2020-05-24 13:24:46 +0900

    0524

--- study/chap3_ex.py
+++ study/chap3_ex.py
@@ -1,4 +1,6 @@
 # Q1
+from study.chap2_ex import result
+
 a = "Life is too short, you need python"
 
 if "wife" in a:
@@ -11,3 +13,21 @@ elif "need" in a:
     print("need")
 else:
     print("none")
+
+# Q2
+num = 0
+result = 0
+while num <= 1000:
+    if(num % 3 ==0):
+        result = result + num
+    num = num + 1
+print(result)
+
+# Q3
+num = 1;
+while num<=5:
+    print("*" * num)
+    num = num + 1
+
+# Q4
+# 실습문제로 바로 넘어가기
